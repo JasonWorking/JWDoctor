2c20bb821b97a9ccaeda304a2a55ca2dff259917
Jason <liujie901223@gmail.com>
2015-04-03 22:48:17 +0800

    Initial Commit

---
+++ JWProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JWProject
+//
+//  Created by Jason on 15/4/3.
+//  Copyright (c) 2015年 JW Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JWProjectTests/JWProjectTests.m
@@ -0,0 +1,40 @@
+//
+//  JWProjectTests.m
+//  JWProjectTests
+//
+//  Created by Jason on 15/4/3.
+//  Copyright (c) 2015年 JW Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface JWProjectTests : XCTestCase
+
+@end
+
+@implementation JWProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
